8622e67154c9dd6aaf45b0621c4d6ae4fd133e2d
ankitasonawane <ankitasonawane1312@gmail.com>
2024-11-02 00:09:36 +0530

    pointer

---
+++ pointerandarray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={10,20,30};
+    cout<<*arr<<endl; //10
+    int *aptr=arr;
+    for(int i=0;i<3;i++){
+        cout<<*aptr<<endl;
+        aptr++;
+    }
+    return 0;
+}
